50dce3be09abba73f7678763ede3f1e2d842c72a
xlouis1100 <49841897+xlouis1100@users.noreply.github.com>
2019-04-21 19:16:24 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,296 @@
+
+const Discord = require('discord.js');
+const Enmap = require(`enmap`);
+const farm = new Enmap();
+const client1 = new Discord.Client();
+
+client1.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client1.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+});
+client1.login(process.env.S1);
+
+const client2 = new Discord.Client();
+client2.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client2.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client2.login(process.env.S2);
+
+const client3 = new Discord.Client();
+client3.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client3.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client3.login(process.env.S3);
+
+const client4 = new Discord.Client();
+client4.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client4.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client4.login(process.env.S4);
+
+
+const client5 = new Discord.Client();
+client5.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client5.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+});
+client5.login(process.env.S5);
+
+const client6 = new Discord.Client();
+client6.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client6.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client6.login(process.env.S6);
+
+const client7 = new Discord.Client();
+client7.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client7.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client7.login(process.env.S7);
+
+const client8 = new Discord.Client();
+client8.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client8.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+});
+client8.login(process.env.S8);
+
+const client9 = new Discord.Client();
+client9.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client9.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+});
+client9.login(process.env.S9);
+
+const client10 = new Discord.Client();
+client10.on("ready", () => {
+var interval = setInterval (function () {
+
+        client10.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client10.login(process.env.S10);
+
+const client11 = new Discord.Client();
+client11.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client11.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+});
+client11.login(process.env.S11);
+
+const client12 = new Discord.Client();
+client12.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client12.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client12.login(process.env.S12);
+
+const client13 = new Discord.Client();
+client13.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client13.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client13.login(process.env.S13);
+
+const client14 = new Discord.Client();
+client14.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client14.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+});
+client14.login(process.env.S14);
+
+const client15 = new Discord.Client();
+client15.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client15.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client15.login(process.env.S15);
+
+const client16 = new Discord.Client();
+client16.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client16.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client16.login(process.env.S16);
+
+const client17 = new Discord.Client();
+client17.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client17.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client17.login(process.env.S17);
+
+const client18 = new Discord.Client();
+client18.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client18.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client18.login(process.env.S18);
+
+const client19 = new Discord.Client();
+client19.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client19.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client19.login(process.env.S19);
+
+const client20 = new Discord.Client();
+client20.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client20.channels.get("569552782844231680").send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+}, 300000);
+
+
+
+
+});
+client20.login(process.env.S20);
+
